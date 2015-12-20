27353aec0e4db687911323fc3a7e5bc29f39991b
Charles Cochrane <cochrane26@gmail.com>
2015-12-20 00:08:16 +0000

    playing around with tcp listener readers

--- server.go
+++ server.go
@@ -3,24 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"net"
-	"net/http"
-	"strings"
-
-	portManager "github.com/charlesworth/tcpServer/portManager"
 )
 
 var port = 8081
 
 func main() {
-	fmt.Println("Launching discovery server...")
-	go discoveryServer()
+	// fmt.Println("Launching discovery server...")
+	// go discoveryServer()
 
 	fmt.Println("Launching tcp server...")
-	for {
-		tcpConnect()
-	}
+	tcpConnect()
 }
 
 func tcpConnect() {
@@ -33,32 +26,43 @@ func tcpConnect() {
 	// run loop forever (or until ctrl-c)
 	for {
 		// will listen for message to process ending in newline (\n)
-		message, err := bufio.NewReader(conn).ReadString('\n')
+		readr := bufio.NewReader(conn)
+		//writr := bufio.NewWriter(conn)
+		//rwer := bufio.NewReadWriter(readr, writr)
+
+		msg, err := readr.ReadString('\n')
 		if err != nil {
 			conn.Close()
 			ln.Close()
 			break
 		}
 		// output message received
-		fmt.Print("Message Received:", string(message))
+		fmt.Print("Message Received:", string(msg))
 		// sample process for string received
-		newmessage := strings.ToUpper(message)
+		//newmessage := strings.ToUpper(msg)
 		// send new string back to client
-		conn.Write([]byte(newmessage + "\n"))
 
+		//fmt.Fprintf(conn, msg+"\n")
+		// conn.Write([]byte("hi \n"))
+		bufio.NewWriter(conn).Write([]byte("hi \n"))
+
+		// _, err = bufio.NewWriter(conn).WriteString(fmt.Sprintln("hi"))
+		// if err != nil {
+		// 	fmt.Println(err)
+		// }
 	}
 }
 
-func discoveryServer() {
-	serv := portManager.New(8100, 8110)
-	i, _ := serv.TakePort()
-	fmt.Println(i)
-	http.HandleFunc("/discover", discoveryHandler)
-	http.ListenAndServe(":8080", nil)
-}
-
-func discoveryHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("hit!")
-	fmt.Fprint(w, port)
-	port++
-}
+// func discoveryServer() {
+// 	serv := portManager.New(8100, 8110)
+// 	i, _ := serv.TakePort()
+// 	fmt.Println(i)
+// 	http.HandleFunc("/discover", discoveryHandler)
+// 	http.ListenAndServe(":8080", nil)
+// }
+//
+// func discoveryHandler(w http.ResponseWriter, r *http.Request) {
+// 	log.Println("hit!")
+// 	fmt.Fprint(w, port)
+// 	port++
+// }
